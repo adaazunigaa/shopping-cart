85a06b9ed8e3fb237c695d03e14cf0ccc3d3184a
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-07-25 15:07:49 -0500

    Added a session to store cart info

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const path = require("path")
 const ejsMate = require("ejs-mate");
 const Bags = require("./models/bags");
+const MongoStore = require('connect-mongo');
+const session  = require('express-session');
+
+const Cart = require("./models/cart");
+
+
 
 const mongoose = require("mongoose");
+const cart = require("./models/cart");
 
 const dbURL = process.env.DB_URL ||  "mongodb://localhost:27017/shoppingCart";
 mongoose.connect(dbURL);
@@ -25,6 +32,39 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.engine("ejs", ejsMate);
+
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbURL,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret,
+    }
+});
+
+store.on("Error", function(e){
+    console.log("session store error", e)
+})
+const sessionConfig = {
+    store,
+    name: "session",
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+
+app.use(function(req, res, next) {
+    res.locals.cart = req.session.cart;
+    next();
+  });
 
 app.get("/", (req, res) => {
     res.render("home")
@@ -48,9 +88,21 @@ app.get("/showBags", async (req, res) => {
     res.render("index.ejs", { bags })
 })
 
-app.post("/updateCart", (req,res)=>{
+app.get("/addToCart/:id", async(req, res)=>{
     
-});
+    const cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    const bag = await Bags.findById(req.params.id)
+    if(!bag){
+        // req.flash("error", "MUA was not found!");
+        return res.redirect("/home")
+    }
+    cart.add(bag, bag._id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect("/showBags");
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
